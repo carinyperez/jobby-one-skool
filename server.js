595b8b7b8d0c926c453727365a62177fbe5932f6
Cariny Perez <perezcariny@gmail.com>
2021-09-07 19:33:33 -0700

    Update server

--- server.js
+++ server.js
@@ -15,15 +15,13 @@ const PORT = process.env.PORT || 5000;
 // app.get('/', (req, res) => res.send(`API is running`));
 
 // serve static assets in production 
-
 if(process.env.NODE_ENV === 'production') {
     // set static folder 
-    app.use(express.static('client', 'build', 'index.html')); 
+    app.use(express.static('client/build')); 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
     })
 }
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); 
 
 // define routes 
